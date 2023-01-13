9b757bc1ccfe89be5ff1d73e08e7c7147207ad9f
David_Motari <davemotari22@gmail.com>
2023-01-13 17:05:43 +0300

    Task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,12 @@
+-- Script that creates a table users
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key 
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters
+
+CREATE TABLE if NOT EXISTS  users (
+	id int NOT NULL AUTO_INCREAMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
+
