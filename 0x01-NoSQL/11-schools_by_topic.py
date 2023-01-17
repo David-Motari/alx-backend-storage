061d90b58ea7bc2d77f8c395a1911c3289735848
under-ground-dog <davemotari22@gmail.com>
2023-01-17 23:51:32 +0300

    Task 11 - find certain document

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+"""
+11-schools_by_topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school having a specific topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
