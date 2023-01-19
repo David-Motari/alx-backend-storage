1136b61294a9dbadbae71e6b0b805b830fb41d0a
under-ground-dog <davemotari22@gmail.com>
2023-01-19 06:07:24 +0300

    Task 5 - Function that tracks how many times a url is visited

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
-"""
-web
-"""
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-
 rdis = redis.Redis()
-num = 0
+count = 0
 
 
 def get_page(url: str) -> str:
     """
-    track how many times a particular URL was accessed in the key "count:{url}"
+    Track how many times a particular URL was accessed in the key "count:{url}"
     and cache the result with an expiration time of 10 seconds.
     """
-    rdis.set(f"cached:{url}", num)
-    reqst = requests.get(url)
-    rdis.incr(f"num:{url}")
-    rdis.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-
+    rdis.set(f"cached:{url}", count)
+    reqst= requests.get(url)
+    rdis.incr(f"count:{url}")
+    rdis.setex(f"cached:{url}", 10, rdis.get(f"cached:{url}"))
     return reqst.text
 
 
 if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
+    get_page('http://slowwly.robertomurray.co.uk')
