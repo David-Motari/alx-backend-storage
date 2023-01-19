4e44ef5b9e6aecb61e0f059d6b0908bdac6e3f8f
under-ground-dog <davemotari22@gmail.com>
2023-01-19 06:08:05 +0300

    Task 5 - Function that tracks how many times a url is visited

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,6 +3,7 @@
     obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
+
 rdis = redis.Redis()
 count = 0
 
@@ -13,11 +14,11 @@ def get_page(url: str) -> str:
     and cache the result with an expiration time of 10 seconds.
     """
     rdis.set(f"cached:{url}", count)
-    reqst= requests.get(url)
+    reqst = requests.get(url)
     rdis.incr(f"count:{url}")
     rdis.setex(f"cached:{url}", 10, rdis.get(f"cached:{url}"))
     return reqst.text
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    get_page("http://slowwly.robertomurray.co.uk")
